f7e9c146526c671e5fc616965a61759e23e551c6
zidany <zidanyteixeira18@gmail.com>
2024-11-20 16:29:54 -0300

    teste pra mobile 1.0

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,34 +1,40 @@
-// src/components/Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import style from './navBar.module.css';
 
 interface NavbarProps {
   handleLogout: () => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ handleLogout }) => (
-  <nav className={style.container}>
-    <ul className={style.lista}>
-      <li className={style.icones}>
-        <Link className={style.link} to="/recent">Animes Recentes</Link>
-      </li>
-      -
-      <li className={style.icones}>
-        <Link className={style.link} to="/favoritos">Favoritos</Link>
-      </li>
-      -
-      <li className={style.icones}>
-        <Link className={style.link} to="/Top10Animes">Top 10</Link>
-      </li>
-      -
-      <li className={style.icones}>
-        <Link className={style.link} to="/anime-list">Lista de Animes</Link>
-      </li>
-    </ul>
-    <button className={style.button} onClick={handleLogout}>Sair</button>
-  </nav>
-);
+const Navbar: React.FC<NavbarProps> = ({ handleLogout }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <nav className={style.container}>
+      <div className={style.left}>
+        <button className={style.hamburger} onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`${style.lista} ${isMenuOpen ? style.open : ''}`}>
+          <li className={style.icones}>
+            <Link className={style.link} to="/recent">Animes Recentes</Link>
+          </li>
+          <li className={style.icones}>
+            <Link className={style.link} to="/favoritos">Favoritos</Link>
+          </li>
+          <li className={style.icones}>
+            <Link className={style.link} to="/Top10Animes">Top 10</Link>
+          </li>
+          <li className={style.icones}>
+            <Link className={style.link} to="/anime-list">Lista de Animes</Link>
+          </li>
+        </ul>
+      </div>
+      <button className={style.button} onClick={handleLogout}>Sair</button>
+    </nav>
+  );
+};
 
 export default Navbar;
